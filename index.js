ea9c6c19ccc12773b83d76a62074d8a927d64a6a
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2021-05-31 16:16:15 +0200

    [ADD] basic bot

---
+++ index.js
@@ -0,0 +1,23 @@
+const { CommandoClient } = require('discord.js-commando');
+const path = require("path");
+
+const client = new CommandoClient({
+    commandPrefix: '!',
+    owner: '243342142775230466',
+    invite: 'https://discord.gg/8nsFQAfH'
+});
+
+client.registry
+    .registerDefaultTypes()
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerGroup('music', 'Music')
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.once('ready', () => {
+    console.log(`Connected as ${client.user.tag} - (${client.user.id})`);
+});
+
+client.on('error', (error) => console.error(error));
+
+client.login("ODQ4OTE0NzIwNTcxNjU0MTY1.YLTjcQ.hHuyjlE_6R9hHuJvck-zmm-HQW4")
