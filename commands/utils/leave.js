0ad6029ffe9b410b03d3d13514cbc13466b959a9
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2021-06-01 16:50:03 +0200

    [ADD] leave command

---
+++ commands/utils/leave.js
@@ -0,0 +1,24 @@
+const { Command, CommandoMessage } = require("discord.js-commando");
+
+module.exports = class JoinCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'leave',
+            group: 'utils',
+            memberName: 'leave',
+            description: 'Join the current channel.',
+        })
+    }
+
+    async run(message) {
+        const voiceChannel = message.member.voice.channel;
+
+        if (!voiceChannel) {
+            return message.say(":x: You must be in a vocal channel.");
+        }
+
+        await voiceChannel.leave();
+
+        return message.say(":thumbsup: Leaving " + "`" + voiceChannel.name + "`")
+    }
+}
--- index.js
+++ index.js
@@ -11,7 +11,6 @@ client.registry
     .registerDefaultTypes()
     .registerDefaultGroups()
     .registerDefaultCommands()
-    .registerGroup('music', 'Music')
     .registerGroup('utils', 'Utils')
     .registerCommandsIn(path.join(__dirname, 'commands'));
 
