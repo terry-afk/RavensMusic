06438cf01875ce37a8c2b7f0fdc309bf4d40d86b
Alexis FABAREZ <alexis.fabarez@epitech.eu>
2021-06-01 11:55:46 +0200

    [ADD] resume commande => to fix

---
+++ commands/music/resume.js
@@ -0,0 +1,30 @@
+const { Command, CommandoMessage } = require("discord.js-commando");
+const { StreamDispatcher } = require("discord.js");
+
+module.exports = class ResumeCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'resume',
+            group: 'music',
+            memberName: 'resume',
+            description: 'Resume the current music.',
+        })
+    }
+
+    async run(message) {
+        const dispatcher = message.client.server.dispatcher;
+        if (!message.member.voice.channel) {
+            return message.say(":x: You must be in a vocal channel.");
+        }
+
+        if (!message.client.voice.connections.first()) {
+            return message.say(":x: I'm not connected. Please use `join` to add me.");
+        }
+
+        if (dispatcher) {
+            dispatcher.resume();
+        }
+
+        return message.say("Playing :notes:")
+    }
+}
